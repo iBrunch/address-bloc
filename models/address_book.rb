1eb5fad3f78cbd78b0ac50d90cc67c3b900c35cb
Scott Arakawa <scottarakawa@gmail.com>
2017-02-03 01:09:25 -0600

    Checkpoint 7 Reading CSVs Complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,5 @@
 require_relative 'entry'
-
+require "csv"
 class AddressBook
   attr_reader :entries
 
@@ -27,5 +27,15 @@ def add_entry(name, phone_number, email)
       index+= 1
   end
   entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers:true, skip_blanks:true)
+
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
   end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -58,4 +58,48 @@ require_relative '../models/address_book'
        end
      end
 
- end
+  context "#import_from_csv" do
+    it "imports the correct number of entries" do
+         book.import_from_csv("entries.csv")
+         book_size = book.entries.size
+
+         expect(book_size).to eq 5
+    end
+  end
+
+    it "imports the 1st entry" do
+      book.import_from_csv("entries.csv")
+      entry_one = book.entries[0]
+
+      check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+    end
+
+
+    it "imports the 2nd entry" do
+      book.import_from_csv("entries.csv")
+
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+    end
+
+    it "imports the 3rd entry" do
+      book.import_from_csv("entries.csv")
+
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+    end
+
+    it "imports the 4th entry" do
+      book.import_from_csv("entries.csv")
+
+      entry_four = book.entries[3]
+      check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
+    end
+
+    it "imports the 5th entry" do
+      book.import_from_csv("entries.csv")
+
+      entry_five = book.entries[4]
+      check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+    end
+end
